df75dd2a8c7910ed2b228a3acb06f7a048369b47
taiberium <taiberium@mail.ru>
2021-05-26 17:29:29 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from './body/body.component'
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: BodyComponent }, //home page
+  
+  
+  { path: "portfolio", component: PortfolioComponent }, //Portfolio
+
+  { path: '**', redirectTo: "" }, //Not found reroute
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopPanelComponent } from './root/top-panel/top-panel.component';
+import { BottomPanelComponent } from './root/bottom-panel/bottom-panel.component';
+import { BodyComponent } from './body/body.component';
+import { Auth0Component } from './auth0/auth0.component';
+import { TableComponent } from './portfolio/table/table.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    RootComponent,
+    TopPanelComponent,
+    BottomPanelComponent,
+    BodyComponent,
+    Auth0Component,
+    TableComponent,
+    PortfolioComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
